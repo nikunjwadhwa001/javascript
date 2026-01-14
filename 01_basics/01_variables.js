e845546af69de27253694ad1c778736fc3246a83
Nikunj Wadhwa <126252813+nikunjwadhwa001@users.noreply.github.com>
2026-01-14 12:05:21 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 263417
+let accountEmail="nikunjwadhwa002@gmail.com"
+var accountPassword="@Nw263417"
+accountCity="Muktsar"
+let accountState;
+
+// Prefer not using var because of issue in block scope and functional scope 
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
