a3be6510060e67a6b867ee1a203c7e49b7446dfb
Nikunj Wadhwa <126252813+nikunjwadhwa001@users.noreply.github.com>
2026-01-16 18:29:30 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // not possible as we are using nodejs not browser
+
+console.log(3+3)
+console.log("Nikunj") // code readability should be high
+
+let name = "Nikunj"
+let age = 21
+let isLinked=true
+let state; //undefined 
+let temp=null; //null
+
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
