c0803aa5dc994192372ff6820855c52d196aef57
Nikunj Wadhwa <126252813+nikunjwadhwa001@users.noreply.github.com>
2026-01-18 20:55:57 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// "true/false" => 1/0
+
+let loggedIn = 1
+
+let booleanLoggedIn=Boolean(loggedIn)
+console.log(booleanLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "nikunj" => true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
